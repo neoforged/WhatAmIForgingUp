12e41572ab4ff660ec1a5d2d8084c5c40afb6de0
Matyrobbrt <matyrobbrt@gmail.com>
2023-05-25 21:02:00 +0300

    4th time's the charm?... I hope anyways

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -91,6 +91,15 @@ object DockerBuild : BuildType({
         }
     }
 
+    params {
+        text("git_branch_spec", """
+                +:refs/heads/(%git_main_branch%)
+                +:refs/heads/(main*)
+                +:refs/heads/(master*)
+                +:refs/heads/(develop|release|staging|main|master)
+            """.trimIndent(), label = "The branch specification of the repository", description = "By default all main branches are build by the configuration. Modify this value to adapt the branches build.", display = ParameterDisplay.HIDDEN, allowEmpty = true)
+    }
+
     steps {
         gradle {
             name = "Build JAR"
