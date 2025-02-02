fcb80c884040ee112d054bcd85e0d67b66150e31
Matyrobbrt <matyrobbrt@gmail.com>
2025-02-02 19:01:54 +0200

    Add more indexes

--- src/main/resources/indexdb/migration/V3__inheritance_indexes.sql
+++ src/main/resources/indexdb/migration/V3__inheritance_indexes.sql
@@ -1,1 +1,4 @@
 create index idx_children_by_parent on class_parents(parent);
+
+create index idx_method_references_by_reference on method_references(reference);
+create index idx_field_references_by_reference on field_references(reference);
