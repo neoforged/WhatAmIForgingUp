81c4fdef8d5a310d5f6f08dbcbccca3f49be024b
Matyrobbrt <matyrobbrt@gmail.com>
2025-03-09 23:54:09 +0200

    Remove alert

--- neoforged-waifuvis-app/src/components/App/App.tsx
+++ neoforged-waifuvis-app/src/components/App/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import {SceneApp, useSceneApp} from '@grafana/scenes';
 import {AppRootProps} from '@grafana/data';
-import {config} from '@grafana/runtime';
-import {Alert} from '@grafana/ui';
-import {DATASOURCE_REF} from '../../constants';
 import {PluginPropsContext} from '../../utils/utils.plugin';
 import {modsPage} from "../../pages/Mods/modsPage.ts";
 
@@ -22,16 +19,6 @@ function AppWithScenes() {
 
   return (
     <>
-      {!config.datasources[DATASOURCE_REF.uid] && (
-        <Alert title={`Missing ${DATASOURCE_REF.uid} datasource`}>
-          These demos depend on <b>testdata</b> datasource: <code>{JSON.stringify(DATASOURCE_REF)}</code>. See{' '}
-          <a href="https://github.com/grafana/grafana/tree/main/devenv#set-up-your-development-environment">
-            https://github.com/grafana/grafana/tree/main/devenv#set-up-your-development-environment
-          </a>{' '}
-          for more details.
-        </Alert>
-      )}
-
       <scene.Component model={scene} />
     </>
   );
